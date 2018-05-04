6f61d2a5e48d1c7a721a7d62b22ecaa68977419a
Paul Stott <pauldavidstott@gmail.com>
2018-05-04 13:08:17 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Toggle } from 'is-ui-library'
+
+class App extends Component {
+  render() {
+    return (
+      <Toggle />
+    );
+  }
+}
+
+export default App;
