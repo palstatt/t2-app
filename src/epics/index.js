8d128cd8726259d06e9e3beccf745ff53585e028
Paul Stott <pauldavidstott@gmail.com>
2018-05-21 09:38:20 -0600

    updated to rxjs 6 pipe functions

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,5 +1,8 @@
 import { Observable } from 'rxjs-compat'
-import { combineEpics } from 'redux-observable'
+import { combineEpics, ofType } from 'redux-observable'
+import { pipe } from 'rxjs'
+import { flatMap, map, catchError, debounceTime, mergeMap, switchMap } from 'rxjs/operators'
+import { ajax } from 'rxjs/ajax'
 
 import {
   ASSIGN_ISSUE,
@@ -28,87 +31,111 @@ const defaultErrorMessage = {
 const responseConfig = { 'Content-Type': 'application/json' }
 
 const handleError = (error, type) => (
-  Observable.of(errorQueryAction(error, type))
+  Observable.from(errorQueryAction(error, type))
 )
+
+// THROW ERROR ON NULL OR REMOVE GETJSON CALL AND HANDLE REQUESTS
+// AFTER FILTERING OUT 404 AND OTHER ERRORS
 
 //epics
 const loginRequestEpic = action$ =>
-  action$.ofType(LOGIN_REQUEST)
-    .flatMap(({payload, type}) =>
-      Observable.ajax(`${process.env.REACT_APP_API_URL}/users?id=${payload}`)
-        .map(res => loginFulfilledAction(res))
-        .catch(error => handleError(error, type))
+  action$.pipe(
+    ofType(LOGIN_REQUEST),
+    flatMap(({payload, type}) =>
+      ajax.getJSON(`${process.env.REACT_APP_API_URL}/users?id=${payload}`).pipe(
+        map(res => loginFulfilledAction(res)),
+        catchError(error => handleError(error, type))
       )
+    )
+  )
 
 const loadAllIssuesEpic = action$ =>
-  action$.ofType(LOAD_ALL_ISSUES)
-    .flatMap(({payload, type}) =>
-      Observable.ajax(`${process.env.REACT_APP_API_URL}/issues`)
-        .map(res => allIssuesLoadedAction(res))
-        .catch(error => handleError(error, type))
+  action$.pipe(
+    ofType(LOAD_ALL_ISSUES),
+    flatMap(({payload, type}) =>
+      ajax.getJSON(`${process.env.REACT_APP_API_URL}/issues`).pipe(
+        map(res => allIssuesLoadedAction(res)),
+        catchError(error => handleError(error, type))
       )
+    )
+  )
 
 const loadIssuesEpic = action$ =>
-  action$.ofType(LOAD_ISSUES)
-    .debounceTime(500)
-    .flatMap(({payload, collectionName, type}) =>
-      Observable.ajax(`${process.env.REACT_APP_API_URL}/${payload}`)
-        .map(res => issuesLoadedAction(res, collectionName))
-        .catch(error => handleError(error, type))
+  action$.pipe(
+    ofType(LOAD_ISSUES),
+    debounceTime(500),
+    flatMap(({payload, collectionName, type}) =>
+      ajax.getJSON(`${process.env.REACT_APP_API_URL}/${payload}`).pipe(
+        map(res => issuesLoadedAction(res, collectionName)),
+        catchError(error => handleError(error, type))
       )
+    )
+  )
 
 const loadAvatarsEpic = action$ =>
-  action$.ofType(LOAD_USERS)
-    .flatMap(({payload, type}) =>
-      Observable.ajax(`${process.env.REACT_APP_API_URL}/users`)
-        .map(url => usersLoadedAction(url))
-        .catch(error => handleError(error, type))
+  action$.pipe(
+    ofType(LOAD_USERS),
+    flatMap(({payload, type}) =>
+      ajax.getJSON(`${process.env.REACT_APP_API_URL}/users`).pipe(
+        map(url => usersLoadedAction(url)),
+        catchError(error => handleError(error, type))
       )
+    )
+  )
 
 const resolveIssueEpic = action$ =>
-   action$.ofType(RESOLVE_ISSUE)
-    .flatMap(({payload, type}) =>
-      Observable.ajax.patch(
-        `${process.env.REACT_APP_API_URL}/issues/${payload}`,
-        { resolved: true },
-        responseConfig
+   action$.pipe(
+     ofType(RESOLVE_ISSUE),
+     flatMap(({payload, type}) =>
+        ajax.patch(
+          `${process.env.REACT_APP_API_URL}/issues/${payload}`,
+          { resolved: true },
+          responseConfig
+        ).pipe(
+          map(({response}) => issueResolvedAction(response)),
+          catchError(error => handleError(error, type))
+        )
       )
-        .map(({response}) => issueResolvedAction(response))
-        .catch(error => handleError(error, type))
   )
 
 const claimIssueEpic = (action$, state$) =>
-  action$.ofType(CLAIM_ISSUE)
-    .flatMap(({payload, type}) =>
-      Observable.ajax.patch(
+  action$.pipe(
+    ofType(CLAIM_ISSUE),
+    flatMap(({payload, type}) =>
+      ajax.patch(
         `${process.env.REACT_APP_API_URL}/issues/${payload}`,
         { claimed: true, assigned_to: state$.value.currentUser.id },
         responseConfig
-      )
-        .mergeMap(({response}) =>
+      ).pipe(
+        mergeMap(({response}) =>
           Observable.of(
             issueClaimedAction(response),
             loadAllIssuesAction()
           )
-        )
-        .catch(error => handleError(error, type))
+        ),
+        catchError(error => handleError(error, type))
+      )
+    )
   )
 
 const assignIssueEpic = action$ =>
-  action$.ofType(ASSIGN_ISSUE)
-    .switchMap(({payload, assignTo, type}) =>
-      Observable.ajax.patch(`${process.env.REACT_APP_API_URL}/issues/${payload}`,
+  action$.pipe(
+    ofType(ASSIGN_ISSUE),
+    switchMap(({payload, assignTo, type}) =>
+      ajax.patch(`${process.env.REACT_APP_API_URL}/issues/${payload}`,
         { claimed: !(assignTo === 0), assigned_to: assignTo },
         responseConfig
-      )
-      .mergeMap(({response}) =>
+      ).pipe(
+      mergeMap(({response}) =>
         Observable.of(
           issueAssignedAction(response),
           loadAllIssuesAction()
         )
-      )
-      .catch(error => handleError(error, type))
+      ),
+      catchError(error => handleError(error, type))
+    )
   )
+)
 
 export const rootEpic = combineEpics(loginRequestEpic,
                                      loadAllIssuesEpic,
